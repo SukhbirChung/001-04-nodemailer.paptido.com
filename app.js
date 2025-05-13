16df4092685fe381b196c530bccb8a5e486b3311
SukhbirChung <sukhbirchung@gmail.com>
2025-05-13 16:55:25 -0400

    Added post request: receives request from star beauty artistry backend app when a new appointmen tis booked and sends an email to the owner notifying a new appointment request is received.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const ejs = require('ejs');
+const fs = require('fs');
 const resetLinkBase = process.env.RESETLINK;
 
 const app = express();
@@ -12,17 +14,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const configurations = {
-    host: process.env.HOSTNAME,
-    port: process.env.PORT,
-    secure: true,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
-};
+function createConfigurations(comingFrom) {
+    const configurations = {
+        host: comingFrom === 'starbeautyartistry' ? process.env.STARBEAUTYARTISTRYHOSTNAME : process.env.HOSTNAME,
+        port: process.env.PORT,
+        secure: true,
+        auth: {
+            user: comingFrom === 'starbeautyartistry' ? process.env.STARBEAUTYARTISTRYEMAIL : process.env.EMAIL,
+            pass: comingFrom === 'starbeautyartistry' ? process.env.STARBEAUTYARTISTRYPASS : process.env.PASS
+        }
+    };
 
-const transporter = nodemailer.createTransport(configurations);
+    return configurations;
+}
 
 function setDestination(comingFrom) {
     if (comingFrom === 'paptido') {
@@ -30,14 +34,16 @@ function setDestination(comingFrom) {
     }
     else if (comingFrom === 'portfolio') {
         return process.env.TOPORTFOLIO;
+    } else if (comingFrom === 'starbeautyartistry') {
+        return process.env.KULDEEPEMAIL;
     }
     return process.env.TO
 }
 
 const sendEmail = (req, res) => {
-    transporter.sendMail(req.mailOptions, function (error, info) {
+    req.transporter.sendMail(req.mailOptions, function (error, info) {
         if (error) {
-            res.send(error);
+            return res.status(500).send({ error: 'Failed to send email' });
         } else {
             res.send(`${req.confirmationCode}`);
         }
@@ -47,18 +53,34 @@ const sendEmail = (req, res) => {
 }
 
 app.post('/', (req, res, next) => {
-    const { comingFrom, name, email, message, subject, course } = req.body;
+    const { comingFrom, name, email, message, subject, phone } = req.body;
+    const configurations = createConfigurations(comingFrom);
+    const transporter = nodemailer.createTransport(configurations);
+
+    let emailText = '';
+    if (email) {
+        emailText = `Email: ${email}`;
+    }
+    let phoneText = '';
+    if (phone) {
+        phoneText = `Phone: ${[phone]}`;
+    }
 
     const mailOptions = {
-        from: process.env.EMAIL,
+        from: comingFrom === 'starbeautyartistry' ? process.env.STARBEAUTYARTISTRYEMAIL : process.env.EMAIL,
         to: setDestination(comingFrom),
-        subject: !comingFrom ? `Form query received: ${course}` : subject,
+        subject: subject,
         text: `Name: ${name}
-Email: ${email}
+${emailText}
+${phoneText}
+
 Message: ${message}`
     };
 
     req.mailOptions = mailOptions;
+    req.transporter = transporter;
+    req.confirmationCode = "Email sent successfully";
+
     next();
 }, sendEmail);
 
@@ -76,51 +98,12 @@ app.post('/confirmation', (req, res, next) => {
 
     req.mailOptions = mailOptions;
     req.confirmationCode = confirmationCode;
+
     next();
 }, sendEmail);
 
-app.post('/studentAccountConfirmation', (req, res, next) => {
-    const email = req.body.email;
-    
-    const paptidoConfigurations = {
-        host: process.env.PAPTIDOHOSTNAME,
-        port: process.env.PORT,
-        secure: true,
-        auth: {
-            user: process.env.PAPTIDOEMAIL,
-            pass: process.env.PAPTIDOPASS
-        }
-    };
-    
-    const paptidoTransporter = nodemailer.createTransport(paptidoConfigurations);
-
-    const confirmationCode = Math.floor(100000 + Math.random() * 900000);
-    const emailBody = `<p style="border-radius: 8px; padding: 8px; text-align: center; background-color: pink;">Your confirmation code is:<br><span style="font-size: 20px;">${confirmationCode}</span></p>
-    <pre style="font-size: 16px; font-family: 'Helvetica', 'Arial', sans-serif;">
-Regards,
-Paptido
-<a href='https://paptido.com'>https://paptido.com</a></pre>`;
-
-    const paptidoMailOptions = {
-        from: `"Paptido" <${process.env.PAPTIDOEMAIL}>`,
-        to: email,
-        subject: 'Confirmation Code',
-        html: emailBody
-    };
-
-    paptidoTransporter.sendMail(paptidoMailOptions, function (error, info) {
-        if (error) {
-            res.status(error.responseCode).send(error.response);
-        } else {
-            res.send(`${confirmationCode}`);
-        }
-    });
-
-    res.header("Access-Control-Allow-Origin", "*");
-});
-
 app.post('/sendResetLink', async (req, res, next) => {
-    const {email, resetToken} = req.body;
+    const { email, resetToken } = req.body;
 
     const resetLink = `${resetLinkBase}?token=${resetToken}`;
     const emailBody = `<p style="border-radius: 8px; padding: 8px; background-color: pink;">Click the following link to reset your password:<br>Link expires in 1 hour.<br><a href="${resetLink}">${resetLink}</a></p>`;
@@ -136,4 +119,32 @@ app.post('/sendResetLink', async (req, res, next) => {
     next();
 }, sendEmail);
 
-app.listen(3000, () => console.log("Listening..."));
+app.post('/starbeautyartistryappointmentrequest', async (req, res, next) => {
+    const { name } = req.body;
+    const configurations = createConfigurations('starbeautyartistry');
+    const transporter = nodemailer.createTransport(configurations);
+
+    // Get access to the ejs template
+    fs.readFile('email-templates/star-beauty-artistry-appointment-request.ejs', 'utf8', (err, template) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const htmlContent = ejs.render(template, req.body);
+        const mailOptions = {
+            from: process.env.STARBEAUTYARTISTRYEMAIL,
+            to: process.env.KULDEEPEMAIL,
+            subject: `${name} sent an appointment request`,
+            html: htmlContent
+        };
+
+        req.mailOptions = mailOptions;
+        req.transporter = transporter;
+        req.confirmationCode = "Appointment request email sent successfully to the owner";
+
+        next();
+    });
+}, sendEmail);
+
+app.listen(3001, () => console.log("Listening..."));
